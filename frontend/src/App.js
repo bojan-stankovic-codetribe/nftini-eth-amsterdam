eeeffe34df15a68630c148676150a19f455adfa2
Bojan Stankovic <bojan.stankovic@codetri.be>
2022-04-23 20:34:24 +0200

    added create album modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,14 @@ import './App.css'
 import { Button } from 'react-bootstrap'
 import useMetaMask from './hooks/metamask';
 import Album from './components/album/Album'
+import CreateAlbum from "./components/createalbum/CreateAlbum";
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 
 function App() {
   
-  const { connect, disconnect, isActive, account, shouldDisable } = useMetaMask()
+  const { connect, disconnect, isActive, account, shouldDisable, library } = useMetaMask()
   const cards = [
     {
       img_url: "https://i.pinimg.com/736x/a5/5a/31/a55a3167f0a068894620b8ef69fd9040.jpg",
@@ -50,19 +53,27 @@ function App() {
       number: 10
     }
   ]
+  console.log(library)
 
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant="secondary" onClick={connect} disabled={shouldDisable}>
-          <img src="images/metamask.svg" alt="MetaMask" width="50" height="50" /> Connect to MetaMask
-        </Button>
+        {!isActive &&
+          <Button variant="secondary" onClick={connect} disabled={shouldDisable}>
+            <img src="images/metamask.svg" alt="MetaMask" width="50" height="50"/> Connect to MetaMask
+          </Button>
+        }
+        {isActive &&
+          <Button variant="danger" onClick={disconnect}>
+            Disconnect MetaMask<img src="images/noun_waving_3666509.svg" width="50" height="50"/>
+          </Button>
+        }
+        {isActive &&
+            <CreateAlbum/>
+        }
         <div className="mt-2 mb-2">
           Connected Account: { isActive ? account : '' }
         </div>
-        <Button variant="danger" onClick={disconnect}>
-          Disconnect MetaMask<img src="images/noun_waving_3666509.svg" width="50" height="50" />
-        </Button>
       </header>
 
       <Album all_cards={cards} user_cards={[1, 2, 3, 6, 7, 8, 10]}/>
